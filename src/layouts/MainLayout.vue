12375214496f5c12b6617b796cec3e17b44eae83
Braden Wong <bmw02002turbo@gmail.com>
2022-08-19 13:29:59 -0700

    Feat: Add Song

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -83,7 +83,7 @@
       <div class="text-center">&copy; Buttery Book 2022</div>
       <q-page-sticky position="bottom-right" :offset="[18, 18]">
         <q-btn fab icon="campaign" @click="reportGeneral()" color="accent"
-          >Suggest</q-btn
+          >Add Song</q-btn
         >
       </q-page-sticky>
     </q-page-container>
